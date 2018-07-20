1bb469cfc067239f26f32f8b93b5e1dafbd27d33
manatlan <manatlan@gmail.com>
2018-07-20 18:28:30 +0200

    coms

--- README.md
+++ README.md
@@ -5,6 +5,10 @@ It's like [python eel](https://github.com/ChrisKnott/Eel) BUT :
 * it's not as polished
 * it uses python3 ONLY
 * it uses asyncio, and the marvellous [aiohttp](https://aiohttp.readthedocs.io/en/stable/)
-* it's server hosted friendly
+* it's server hosted friendly from scratch
 * it uses pubsub mechanism to communicate from server to clients, or from client to clients
 * it's not pyinstaller-friendly for now
+* it doesn't exit when no sockets anymore
+* com errors are catch'able thru the promise
+
+it's a proof of concept : HUY means **H**tml **U**ser **Y**nterface ...
--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 from aiohttp import web
 import asyncio
 import json,sys,os
@@ -10,19 +12,19 @@ except:
     except:
         pass
 
-clients=[]
+clients=[] #<- for saving clients cnx
 
 async def as_emit(event,args,exceptMe=None):
     global clients
     for ws in clients:
         if id(ws) != id(exceptMe):
-            print("emit '%s' : %s" % (event,args))
+            print("emit event '%s' : %s" % (event,args))
             await ws.send_str( json.dumps( dict(event=event,args=args) ))
 
-def emit(event,args):
+def emit(event,args):   # sync version of emit for py side !
     asyncio.ensure_future( as_emit( event, args) )
 
-async def handle(request):
+async def handle(request): # serve all statics from web folder
     file = './web/'+request.match_info.get('path', "index.html")
     if os.path.isfile(file):
         return web.FileResponse(file)
@@ -44,8 +46,8 @@ async def wshandle(request):
                 print("RECEPT",o)
                 if o["command"] == "emit":
                     event, *args = o["args"]
-                    await as_emit( event, args, ws)
-                    r=dict(result = args)
+                    await as_emit( event, args, ws) # emit to everybody except me
+                    r=dict(result = args)           # but return the same content sended, thru the promise
                 else:
                     pythonMethod = getattr(sys.modules[__name__], o["command"])
                     r=dict(result = pythonMethod( *o["args"] ) )
@@ -69,12 +71,12 @@ def my_python_method(a,b):
     return a+b
 
 def my_python_method2(a):
-    emit( "js_event", "from python sync")
+    emit( "js_event", "from python sync")   # emit an event to all clients (me too !)
     return a*2
 
 async def handleTest(request):
     await as_emit( "js_event", "from python async")
-    return web.Response(body="ok")
+    return web.Response(body="I sent a event to all client, from this python web page !")
 
 if __name__=="__main__":
     app = web.Application()
