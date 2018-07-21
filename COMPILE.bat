7e8df291a2322a497555003b47923383b26bf593
manatlan <manatlan@gmail.com>
2018-07-21 16:07:18 +0200

    better compile bat

--- COMPILE.bat
+++ COMPILE.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
-c:\Python3\Scripts\pyinstaller.exe --noupx --onefile server.py --add-data="web/index.html;web"
+c:\Python3\Scripts\pyinstaller.exe --noupx --onefile --noconsole server.py --add-data="web/index.html;web"
 REM ~ c:\Python3\Scripts\pyinstaller.exe web.spec
 pause
