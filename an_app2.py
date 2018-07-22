08161e45de168da5d97a9198173cfb02604a7f36
manatlan <manatlan@gmail.com>
2018-07-22 14:06:51 +0200

    better test

--- an_app2.py
+++ an_app2.py
@@ -4,16 +4,18 @@ import wuy
 # it's the future ... (just ask name and return it)
 
 class askName(wuy.Window):    # name the class as the web/<class_name>.html
-    size=(200,200)
+    size=(200,70)
     
-    name=None
+    name=None   # <- a placeholder
 
     def post(self,name):
-        self.name=name.strip()
-        if self.name:
+        if name.strip():
+            self.name=name.strip()
             self.exit()
-
 
 if __name__=="__main__":
     x=askName()
-    print("Your name is '%s' !!!" % x.name)
+    if x.name:
+        print("Your name is '%s' !!!" % x.name)
+    else:
+        print("You are unamed ?! ;-) ")
