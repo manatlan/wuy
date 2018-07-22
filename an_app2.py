25ae58006420265315291a4652810c75c8174cab
manatlan <manatlan@gmail.com>
2018-07-22 12:39:54 +0200

    test name

--- an_app2.py
+++ an_app2.py
@@ -9,10 +9,11 @@ class askName(wuy.Window):    # name the class as the web/<class_name>.html
     name=None
 
     def post(self,name):
-        self.name=name
-        self.exit()
+        self.name=name.strip()
+        if self.name:
+            self.exit()
 
 
 if __name__=="__main__":
     x=askName()
-    print("Your name is",x.name,"!!!")
+    print("Your name is '%s' !!!" % x.name)
