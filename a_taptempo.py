c2bbb6f14afeb8f1bfbb2977238a3df5e3725cb9
manatlan <manatlan@gmail.com>
2018-08-19 16:36:39 +0200

    add a taptempo example ;-)

---
+++ a_taptempo.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import wuy,datetime
+
+# use docstring as content for the html response
+
+class taptempo(wuy.Window):
+    """ <button onclick="wuy.tic().then(x=>{document.querySelector('#tempo').innerHTML=x})">Tap Tempo</button> 
+        <span id="tempo"></span>
+    """
+    size=(100,60)
+    t=[]
+
+    def tic(self):
+        self.t.append( datetime.datetime.now() )
+        ll=[ (j-i).microseconds for i, j in zip(self.t[:-1], self.t[1:]) ][-5:]
+        if ll: 
+            return int(60000000*len(ll)/sum(ll))
+
+if __name__=="__main__":
+    taptempo()
