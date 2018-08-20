52e12b93fd248a1ca4ff8455cc697bfd71a98364
manatlan <manatlan@gmail.com>
2018-08-20 10:13:12 +0200

    win don't like web:web

--- BUILD.bat
+++ BUILD.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
-c:\Python3\Scripts\pyinstaller.exe an_app.py --noupx --onefile --noconsole --add-data="web:web"
+c:\Python3\Scripts\pyinstaller.exe an_app.py --noupx --onefile --noconsole --add-data="web;web"
 REM $ pyinstaller an_app.py --noupx --onefile --noconsole --add-data="web:web"
 pause
